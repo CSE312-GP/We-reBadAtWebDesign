31cdbb2999a2c38cc108affd3269dce862ea93a3
AaronMango <avmangin@buffalo.edu>
2024-03-12 17:21:09 -0400

    Working docker

---
+++ app.py
@@ -0,0 +1,13 @@
+#Backend Python for our server
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_geek():
+    
+    return '<h1>Hello from Flask & Docker</h1>'
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=8080)
