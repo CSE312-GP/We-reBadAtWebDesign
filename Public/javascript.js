e57e0b49e02e2fe521b0f126fe01e01eb463bfc7
Eric Hildreth <erichild@buffalo.edu>
2024-03-24 20:53:40 -0400

    added functions to javascript

--- Public/javascript.js
+++ Public/javascript.js
@@ -1,3 +1,19 @@
 function addText() {
     document.getElementById("head").innerHTML += "Otaku Forum: Review, Share, Connect";
 }
+
+function addMessageToChat(messageJSON) {
+    const chatMessages = document.getElementById("chat-messages");
+    chatMessages.innerHTML += chatMessageHTML(messageJSON);
+    chatMessages.scrollIntoView(false);
+    chatMessages.scrollTop = chatMessages.scrollHeight - chatMessages.clientHeight;
+}
+
+function chatMessageHTML(messageJSON) {
+    const username = messageJSON.username;
+    const message = messageJSON.message;
+    const messageId = messageJSON.id;
+    let messageHTML = "<br><button onclick='deleteMessage(\"" + messageId + "\")'>X</button> ";
+    messageHTML += "<span id='message_" + messageId + "'><b>" + username + "</b>: " + message + "</span>";
+    return messageHTML;
+}
